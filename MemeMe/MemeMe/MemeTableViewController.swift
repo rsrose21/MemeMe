241b0fb4e637bf6a58790274a75682155d8cc495
Ryan Rose <ryan.s.rose@ge.com>
2015-05-18 22:05:47 -0400

    Added table view and tab bar

---
+++ MemeMe/MemeMe/MemeTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MemeTableViewController.swift
+//  MemeMe
+//
+//  Created by Ryan Rose on 5/18/15.
+//  Copyright (c) 2015 GE. All rights reserved.
+//
+
+import UIKit
+
+class MemeTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var memes: [Meme]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    //table delegate methods
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //identify number of rows in table based on number of Memes in array
+        return (UIApplication.sharedApplication().delegate as! AppDelegate).memes.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        //set cell data for each row in table
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("MemeCell", forIndexPath: indexPath) as! UITableViewCell
+        cell.imageView?.image = (UIApplication.sharedApplication().delegate as! AppDelegate).memes[indexPath.row].memedImage
+        cell.textLabel?.text = (UIApplication.sharedApplication().delegate as! AppDelegate).memes[indexPath.row].topText
+        
+        return cell
+    }
+}
